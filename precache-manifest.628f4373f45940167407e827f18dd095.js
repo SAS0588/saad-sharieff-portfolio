ddac18b393f919bd68cb2be2e948262239cbdce2
SAS0588 <saadsharieff@gmail.com>
2020-08-02 14:21:05 -0500

    Updates

---
+++ precache-manifest.628f4373f45940167407e827f18dd095.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f388ae61ec6840c6145b1e725eca5d3",
+    "url": "/saad-sharieff-portfolio/index.html"
+  },
+  {
+    "revision": "dc857fbdd04eac55fe97",
+    "url": "/saad-sharieff-portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "45ba63d8aeaf613a7ea4",
+    "url": "/saad-sharieff-portfolio/static/css/main.ca7a82b9.chunk.css"
+  },
+  {
+    "revision": "dc857fbdd04eac55fe97",
+    "url": "/saad-sharieff-portfolio/static/js/2.05c17f90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/saad-sharieff-portfolio/static/js/2.05c17f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ba63d8aeaf613a7ea4",
+    "url": "/saad-sharieff-portfolio/static/js/main.1df4c544.chunk.js"
+  },
+  {
+    "revision": "db3d71c4501054eab3e4",
+    "url": "/saad-sharieff-portfolio/static/js/runtime-main.b4bd5ea0.js"
+  },
+  {
+    "revision": "dd915b488d8c6fd10697df59298a888c",
+    "url": "/saad-sharieff-portfolio/static/media/Resume.dd915b48.pdf"
+  },
+  {
+    "revision": "1b6469585068ac838cc67c7e9079dfdc",
+    "url": "/saad-sharieff-portfolio/static/media/logo_small.1b646958.svg"
+  }
+]);
